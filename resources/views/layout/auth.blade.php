0c62c07205c2dde95a4bcf03fa69641a445f5c20
Fani Annur Rozi <crazyhx4@gmail.com>
2025-10-13 05:06:23 +0700

    layout untuk halaman auth

---
+++ resources/views/layout/auth.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>@yield('title', 'Online Shop')</title>
+        @vite(['resources/css/app.css', 'resources/js/auth.js'])
+    </head>
+
+    <body
+        x-data="{ page: 'ecommerce', 'loaded': true, 'darkMode': false, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }"
+        x-init="
+            darkMode = JSON.parse(localStorage.getItem('darkMode'));
+            $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
+        :class="{'dark bg-gray-900': darkMode === true}">
+        @yield('content')
+    </body>
+</html>
